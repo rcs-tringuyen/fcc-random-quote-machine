d8e1f161b4e5cfef2afb307ed23d28de7d4cbc04
TriNguyen <tlnguyen@ualberta.ca>
2022-01-03 19:23:04 -0700

    Fetching random quote from https://api.quotable.io/random

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
+function QuoteBox({ quote }) {
+  return (
+    <div className="quote-box">
+      <div id='text'>
+        {quote.content}
+      </div>
+      <div id='author'>
+        {quote.author}
+      </div>
+      <div id='new-quote'>
+        <button>New Quote</button>
+      </div>
+      <a href='' id='tweet-quote'>Tweet!</a>
+    </div>
+  )
+}
+
 function App() {
+
+  const [quote, setQuote] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://api.quotable.io/random")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setQuote(result);
+        }
+      )
+  }, []);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QuoteBox
+        quote={quote}
+      />
     </div>
   );
 }
